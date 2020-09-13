d594c2b116a262e9051ae953762b6a681dfc487f
khaledtag <khaled1544_1@yahoo.com>
2020-09-14 00:32:28 +0200

    switch button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <DarkMode />
     <Sammary />
     <StaySafe />
     <NavBar />
@@ -9,13 +10,14 @@
 <script>
 import Sammary from "@/components/Sammary.vue";
 import NavBar from "@/components/NavBar.vue";
-
+import DarkMode from "@/components/DarkMode.vue";
 import StaySafe from "@/components/staySafe.vue";
 export default {
   name: "App",
   components: {
     Sammary,
     NavBar,
+    DarkMode,
     StaySafe
   }
 };
@@ -25,76 +27,77 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
 
 body {
-    /* #edf2f6 */
-    background-color: #fdf8f5 !important;
-    /* background: rgb(0,168,255);
+  /* #edf2f6 */
+  background-color: #fdf8f5 !important;
+  /* background: rgb(0,168,255);
   background: radial-gradient(circle, rgba(0,168,255,1) 2%, rgba(8,51,179,1) 68%, rgba(0,45,176,1) 89%); */
 }
 
 body::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
-
 
 /* Hide scrollbar for IE and Edge */
 
 body {
-    -ms-overflow-style: none;
+  -ms-overflow-style: none;
 }
-
 
 /* reseting */
 
 p {
-    margin-top: initial !important;
-    margin-bottom: initial !important;
+  margin-top: initial !important;
+  margin-bottom: initial !important;
 }
 
 ul {
-    list-style: none;
-    margin: 0 !important;
-    padding: 0 !important;
+  list-style: none;
+  margin: 0 !important;
+  padding: 0 !important;
 }
-
 
 /* reseting */
 
 .v-dropdown-container {
-    height: 400px;
-    overflow-y: scroll;
+  height: 400px;
+  overflow-y: scroll;
 }
 
 #app {
-    font-family: "Open Sans", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    /* text-align: center; */
-    color: #2c3e50;
+  font-family: "Open Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
 }
 
 .header {
-    font-size: 70px;
-    color: #fff;
+  font-size: 70px;
+  color: #fff;
 }
 
 .v-select-toggle {
-    height: 50px;
-    font-size: 16px !important;
-    align-items: center;
-    color: #818991 !important;
-    outline: none !important;
-    background-color: #fff !important;
+  height: 50px;
+  font-size: 16px !important;
+  align-items: center;
+  color: #818991 !important;
+  outline: none !important;
+  background-color: #fff !important;
 }
 
 .v-select-toggle:hover {
-    box-shadow: 0 0px 1.7px rgba(0, 0, 0, 0.02), 0 0px 4.1px rgba(0, 0, 0, 0.028), 0 0px 7.8px rgba(0, 0, 0, 0.035), 0 0px 13.8px rgba(0, 0, 0, 0.042), 0 0px 25.9px rgba(0, 0, 0, 0.05), 0 0px 62px rgba(0, 0, 0, 0.07);
+  box-shadow: 0 0px 1.7px rgba(0, 0, 0, 0.02), 0 0px 4.1px rgba(0, 0, 0, 0.028),
+    0 0px 7.8px rgba(0, 0, 0, 0.035), 0 0px 13.8px rgba(0, 0, 0, 0.042),
+    0 0px 25.9px rgba(0, 0, 0, 0.05), 0 0px 62px rgba(0, 0, 0, 0.07);
 }
 
 .v-select-toggle:focus {
-    box-shadow: 0 0px 1.7px rgba(0, 0, 0, 0.02), 0 0px 4.1px rgba(0, 0, 0, 0.028), 0 0px 7.8px rgba(0, 0, 0, 0.035), 0 0px 13.8px rgba(0, 0, 0, 0.042), 0 0px 25.9px rgba(0, 0, 0, 0.05), 0 0px 62px rgba(0, 0, 0, 0.07);
+  box-shadow: 0 0px 1.7px rgba(0, 0, 0, 0.02), 0 0px 4.1px rgba(0, 0, 0, 0.028),
+    0 0px 7.8px rgba(0, 0, 0, 0.035), 0 0px 13.8px rgba(0, 0, 0, 0.042),
+    0 0px 25.9px rgba(0, 0, 0, 0.05), 0 0px 62px rgba(0, 0, 0, 0.07);
 }
 
 .v-dropdown-container ul {
-    font-size: 14px !important;
+  font-size: 14px !important;
 }
 </style>
--- src/icons.js
+++ src/icons.js
@@ -1,23 +1,12 @@
-import Vue from "vue"
-import {
-    library
-} from "@fortawesome/fontawesome-svg-core"
-import {
-    FontAwesomeIcon
-} from "@fortawesome/vue-fontawesome"
+import Vue from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 // fas to import all icons (not recommended)
-import {
-    faSearch,
-    faInfo,
-    faArrowUp,
-
-} from "@fortawesome/free-solid-svg-icons"
+import { faSearch, faInfo, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import {
     faFacebookSquare,
     faLinkedin,
-} from "@fortawesome/free-brands-svg-icons"
+} from "@fortawesome/free-brands-svg-icons";
 
-
-
-library.add(faFacebookSquare, faLinkedin, faSearch, faInfo, faArrowUp)
-Vue.component('fa-icon', FontAwesomeIcon)
+library.add(faFacebookSquare, faLinkedin, faSearch, faInfo, faArrowUp);
+Vue.component("fa-icon", FontAwesomeIcon);
